c9b0bb468a24bfa69b591267de8df4b56f988572
Drew Williams <williams.r.drew@gmail.com>
2019-09-09 17:06:55 -0400

    Hello JavaFX

---
+++ src/test/java/edu/buffalo/cse442/nullterminators/MainTest.java
@@ -0,0 +1,12 @@
+package edu.buffalo.cse442.nullterminators;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
