f4ac9f20d4b6b6a96e0c7be3eecf812c3ec72815
Drew Williams <williams.r.drew@gmail.com>
2019-09-22 13:18:13 -0400

    Different views working. Visual bug with first col

--- src/main/java/edu/buffalo/cse442/nullterminators/MainWindow.java
+++ src/main/java/edu/buffalo/cse442/nullterminators/MainWindow.java
@@ -1,6 +1,9 @@
 package edu.buffalo.cse442.nullterminators;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.time.LocalDate;
@@ -9,6 +12,9 @@ public class MainWindow {
 
     @FXML private CalendarNode calendarGrid;
     @FXML private DateDisplay dateDisplayController;
+    @FXML private Button weekViewButton;
+    @FXML private Button monthViewButton;
+    @FXML private Button dayViewButton;
 
     public MainWindow() {
     }
@@ -16,5 +22,25 @@ public class MainWindow {
     @FXML
     void initialize() {
         dateDisplayController.updateDateText(LocalDate.now());
+
+        weekViewButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                calendarGrid.changeView(CalendarNode.VIEW.WEEK);
+            }
+        });
+        monthViewButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                calendarGrid.changeView(CalendarNode.VIEW.MONTH);
+            }
+        });
+        dayViewButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                calendarGrid.changeView(CalendarNode.VIEW.DAY);
+            }
+        });
     }
+
 }
