8db9cc1a815e67eefe74d1fe593f86321400d74d
Drew Williams <williams.r.drew@gmail.com>
2019-11-10 21:43:12 -0500

    fixed tree layout of toggle

--- src/main/java/edu/buffalo/cse442/nullterminators/SideBar.java
+++ src/main/java/edu/buffalo/cse442/nullterminators/SideBar.java
@@ -46,7 +46,7 @@ public class SideBar extends Stage {
 
         VBox tagContainer = new VBox();
         tagContainer.setPadding(new Insets(10,10,10,10));
-        tagContainer.setAlignment(Pos.CENTER);
+        //tagContainer.setAlignment(Pos.CENTER);
         tagContainer.setSpacing(5);
         CheckBox group = new CheckBox();
         for (Tag t : tags) {
